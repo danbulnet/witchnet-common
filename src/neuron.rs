051c2b53b41103c9c1ad8d91bedc79c2dd29630d
danbulnet <daniel@bulanda.net>
2022-08-28 21:39:08 +0200

    rename get_activation -> activation

--- src/neuron.rs
+++ src/neuron.rs
@@ -7,7 +7,7 @@ pub struct NeuronID {
 pub trait Neuron {
     fn get_id(&self) -> NeuronID;
 
-    fn get_activation(&self) -> f32;
+    fn activation(&self) -> f32;
 
     fn stimulate(
         &mut self, signal: f32, propagate_horizontal: bool, propagate_vertical: bool
