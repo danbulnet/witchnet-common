41c853b6633f13cd08751bb62ee30997603d85d4
danbulnet <daniel@bulanda.net>
2022-08-31 03:55:43 +0200

    implement hash for dyn Neuron

--- src/neuron.rs
+++ src/neuron.rs
@@ -7,7 +7,7 @@ use std::{
 
 use crate::connection::{ Connection, ConnectionKind };
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash)]
 pub struct NeuronID {
     pub id: Rc<str>,
     pub parent_id: Rc<str>
@@ -37,16 +37,33 @@ pub trait NeuronConnect {
     ) -> Result<Rc<RefCell<dyn Connection<From = Self, To = dyn Neuron>>>, String>;
 }
 
-struct RcRefCellNeuronHash(Rc<RefCell<dyn Neuron>>);
+// struct RcRefCellNeuronHash(Rc<RefCell<dyn Neuron>>);
 
-impl PartialEq for RcRefCellNeuronHash {
-    fn eq(&self, other: &RcRefCellNeuronHash) -> bool { Rc::ptr_eq(&self.0, &other.0) }
+// impl PartialEq for RcRefCellNeuronHash {
+//     fn eq(&self, other: &RcRefCellNeuronHash) -> bool { Rc::ptr_eq(&self.0, &other.0) }
+// }
+
+// impl Eq for RcRefCellNeuronHash {}
+
+// impl Hash for RcRefCellNeuronHash {
+//     fn hash<H>(&self, hasher: &mut H) where H: Hasher { 
+//         hasher.write_usize(Rc::as_ptr(&self.0) as *const () as usize);
+//     }
+// }
+
+impl PartialEq for dyn Neuron {
+    fn eq(&self, other: &dyn Neuron) -> bool { 
+        let left = self.id(); 
+        let right = other.id();
+        left.id == right.id && left.parent_id == right.parent_id
+    }
 }
 
-impl Eq for RcRefCellNeuronHash {}
+impl Eq for dyn Neuron {}
 
-impl Hash for RcRefCellNeuronHash {
-    fn hash<H>(&self, hasher: &mut H) where H: Hasher { 
-        hasher.write_usize(Rc::as_ptr(&self.0) as *const () as usize);
+impl Hash for dyn Neuron {
+    fn hash<H>(&self, mut hasher: &mut H) where H: Hasher { 
+        self.id().hash(&mut hasher);
+        hasher.finish();
     }
 }
