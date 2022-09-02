4653fda4d5efdc6f45bb4d4b31f22adaa9e86cef
danbulnet <daniel@bulanda.net>
2022-09-02 15:51:50 +0200

    replace SensorDataType with Any

--- src/sensor.rs
+++ src/sensor.rs
@@ -1,7 +1,8 @@
 use std::{
     rc::Rc,
     cell::RefCell,
-    collections::HashMap
+    collections::HashMap,
+    any::Any
 };
 
 use crate::{
@@ -9,28 +10,25 @@ use crate::{
     data::DataCategory
 };
 
-pub trait SensorDataType {}
-impl<T> SensorDataType for T {}
-
 pub trait Sensor {
     fn name(&self) -> &str;
 
     fn data_category(&self) -> DataCategory;
     
-    fn insert(&mut self, item: &dyn SensorDataType) -> Rc<RefCell<dyn Neuron>>;
+    fn insert(&mut self, item: &dyn Any) -> Rc<RefCell<dyn Neuron>>;
     
-    fn search(&self, item: &dyn SensorDataType) -> Option<Rc<RefCell<dyn Neuron>>>;
+    fn search(&self, item: &dyn Any) -> Option<Rc<RefCell<dyn Neuron>>>;
 
     fn activate(
         &mut self, 
-        item: &dyn SensorDataType, 
+        item: &dyn Any, 
         signal: f32, 
         propagate_horizontal: bool, 
         propagate_vertical: bool
     ) -> Result<HashMap<NeuronID, Rc<RefCell<dyn Neuron>>>, String>;
     
     fn deactivate(
-        &mut self, item: &dyn SensorDataType, propagate_horizontal: bool, propagate_vertical: bool
+        &mut self, item: &dyn Any, propagate_horizontal: bool, propagate_vertical: bool
     ) -> Result<(), String>;
 
     fn deactivate_sensor(&mut self);
