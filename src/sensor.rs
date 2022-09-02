dd030d3b819dc6c210fe16a623b88c8d1c6fb95a
danbulnet <daniel@bulanda.net>
2022-09-02 22:43:13 +0200

    combine DataType and SensorMarker

--- src/sensor.rs
+++ src/sensor.rs
@@ -2,33 +2,38 @@ use std::{
     rc::Rc,
     cell::RefCell,
     collections::HashMap,
-    any::Any
+    fmt::Display
 };
 
 use crate::{
     neuron::{ Neuron, NeuronID },
-    data::DataCategory
+    data::DataCategory,
+    distances::Distance
 };
 
+pub trait SensorData: Clone + Display + PartialOrd + PartialEq + Distance {}
+
 pub trait Sensor where  {
+    type Data: SensorData;
+
     fn name(&self) -> &str;
 
     fn data_category(&self) -> DataCategory;
     
-    fn insert(&mut self, item: &dyn Any) -> Rc<RefCell<dyn Neuron>>;
+    fn insert(&mut self, item: &Self::Data) -> Rc<RefCell<dyn Neuron>>;
     
-    fn search(&self, item: &dyn Any) -> Option<Rc<RefCell<dyn Neuron>>>;
+    fn search(&self, item: &Self::Data) -> Option<Rc<RefCell<dyn Neuron>>>;
 
     fn activate(
         &mut self, 
-        item: &dyn Any, 
+        item: &Self::Data, 
         signal: f32, 
         propagate_horizontal: bool, 
         propagate_vertical: bool
     ) -> Result<HashMap<NeuronID, Rc<RefCell<dyn Neuron>>>, String>;
     
     fn deactivate(
-        &mut self, item: &dyn Any, propagate_horizontal: bool, propagate_vertical: bool
+        &mut self, item: &Self::Data, propagate_horizontal: bool, propagate_vertical: bool
     ) -> Result<(), String>;
 
     fn deactivate_sensor(&mut self);
