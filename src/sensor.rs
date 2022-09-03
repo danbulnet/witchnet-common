55a4839f4cc4060834aefb832bf55398de34600b
danbulnet <daniel@bulanda.net>
2022-09-03 19:55:53 +0200

    replace dyn SensorData with associated type for Sensor trait

--- src/sensor.rs
+++ src/sensor.rs
@@ -90,24 +90,26 @@ impl PartialOrd for dyn SensorData + '_ {
 // where T: Clone + Display + PartialOrd + PartialEq + Distance {}
 
 pub trait Sensor {
+    type Data: SensorData; 
+
     fn name(&self) -> &str;
 
     fn data_category(&self) -> DataCategory;
     
-    fn insert(&mut self, item: &dyn SensorData) -> Rc<RefCell<dyn Neuron>>;
+    fn insert(&mut self, item: &Self::Data) -> Rc<RefCell<dyn Neuron>>;
     
-    fn search(&self, item: &dyn SensorData) -> Option<Rc<RefCell<dyn Neuron>>>;
+    fn search(&self, item: &Self::Data) -> Option<Rc<RefCell<dyn Neuron>>>;
 
     fn activate(
         &mut self, 
-        item: &dyn SensorData, 
+        item: &Self::Data, 
         signal: f32, 
         propagate_horizontal: bool, 
         propagate_vertical: bool
     ) -> Result<HashMap<NeuronID, Rc<RefCell<dyn Neuron>>>, String>;
     
     fn deactivate(
-        &mut self, item: &dyn SensorData, propagate_horizontal: bool, propagate_vertical: bool
+        &mut self, item: &Self::Data, propagate_horizontal: bool, propagate_vertical: bool
     ) -> Result<(), String>;
 
     fn deactivate_sensor(&mut self);
