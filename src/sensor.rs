e24e968bedbc9aa83913a43d1847d8087db75d67
danbulnet <daniel@bulanda.net>
2022-09-03 15:35:30 +0200

    impl SensorDataMarker for String and str

--- src/sensor.rs
+++ src/sensor.rs
@@ -20,7 +20,8 @@ macro_rules! impl_sensor_data_marker {
 impl_sensor_data_marker! { 
     i8, i16, i32, i64, i128, isize,
     u8, u16, u32, u64, u128, usize,
-    f32, f64
+    f32, f64,
+    String, str
 }
 
 pub trait SensorData: Clone + Display + PartialOrd + PartialEq + Distance {}
