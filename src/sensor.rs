072175516232f1f37dc39196d78133ef76e312a6
danbulnet <daniel@bulanda.net>
2022-09-03 19:45:18 +0200

    implememnt SensorData as trait object safe

--- src/sensor.rs
+++ src/sensor.rs
@@ -2,7 +2,9 @@ use std::{
     rc::Rc,
     cell::RefCell,
     collections::HashMap,
-    fmt::Display 
+    fmt::Display ,
+    any::Any,
+    cmp::Ordering
 };
 
 use crate::{
@@ -24,32 +26,88 @@ impl_sensor_data_marker! {
     String, str
 }
 
-pub trait SensorData: Clone + Display + PartialOrd + PartialEq + Distance {}
+pub trait SensorData: Display {
+    fn any(&self) -> &dyn Any;
+    fn distance(&self, v: &dyn SensorData) -> f64;
+    fn equals(&self, rhs: &dyn SensorData) -> bool;
+    fn partial_cmp(&self, rhs: &dyn SensorData) -> Option<Ordering>;
+}
 
-impl<T> SensorData for T 
-where T: Clone + Display + PartialOrd + PartialEq + Distance {}
+impl<T: Display + PartialOrd + PartialEq + 'static> SensorData for T {
+    fn any(&self) -> &dyn Any { self }
+
+    fn distance(&self, rhs: &dyn SensorData) -> f64 {
+        if *self == *rhs.any().downcast_ref::<T>().unwrap() { 0.0 } else { 1.0 }
+    }
+
+    fn equals(&self, rhs: &dyn SensorData) -> bool {
+        rhs.any().downcast_ref::<T>().map(|rhs| rhs == self).unwrap_or(false)
+    }
+    
+    fn partial_cmp(&self, rhs: &dyn SensorData) -> Option<Ordering> {
+        self.partial_cmp(rhs.any().downcast_ref::<T>().unwrap())
+    }
+}
+
+impl Eq for dyn SensorData {}
+
+impl PartialEq for dyn SensorData + '_ { 
+    fn eq(&self, rhs: &Self) -> bool { self.equals(rhs) }
+ }
+
+impl PartialOrd for dyn SensorData + '_ {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> { 
+        self.partial_cmp(other) 
+    }
+}
+
+// pub trait DistanceX where {
+//     fn any(&self) -> &dyn Any;
+//     fn distance(&self, v: &dyn DistanceX) -> f64;
+//     fn equals(&self, rhs: &dyn DistanceX) -> bool;
+// }
+
+// impl<T: 'static + PartialEq> DistanceX for T {
+//     // Co-opting the compiler's inference was easier than figuring out the
+//     // direct incantation.
+//     fn any(&self) -> &dyn Any {
+//         self
+//     }
+
+//     // Note here that we're downcasting to T and not dyn FooTrait or Box<dyn FooTrait>
+//     fn distance(&self, rhs: &dyn DistanceX) -> f64 {
+//         if *self == *rhs.any().downcast_ref::<T>() { 0.0 } else { 1.0 }
+//     }
+
+//     fn equals(&self, rhs: &dyn DistanceX) -> bool {
+//         rhs.any().downcast_ref::<T>().map(|rhs| rhs == self).unwrap_or(false)
+//     }
+// }
+
+// pub trait SensorData: Clone + Display + PartialOrd + PartialEq + Distance {}
+
+// impl<T> SensorData for T 
+// where T: Clone + Display + PartialOrd + PartialEq + Distance {}
 
 pub trait Sensor {
-    type Data: Distance;
-
     fn name(&self) -> &str;
 
     fn data_category(&self) -> DataCategory;
     
-    fn insert(&mut self, item: &Self::Data) -> Rc<RefCell<dyn Neuron>>;
+    fn insert(&mut self, item: &dyn SensorData) -> Rc<RefCell<dyn Neuron>>;
     
-    fn search(&self, item: &Self::Data) -> Option<Rc<RefCell<dyn Neuron>>>;
+    fn search(&self, item: &dyn SensorData) -> Option<Rc<RefCell<dyn Neuron>>>;
 
     fn activate(
         &mut self, 
-        item: &Self::Data, 
+        item: &dyn SensorData, 
         signal: f32, 
         propagate_horizontal: bool, 
         propagate_vertical: bool
     ) -> Result<HashMap<NeuronID, Rc<RefCell<dyn Neuron>>>, String>;
     
     fn deactivate(
-        &mut self, item: &Self::Data, propagate_horizontal: bool, propagate_vertical: bool
+        &mut self, item: &dyn SensorData, propagate_horizontal: bool, propagate_vertical: bool
     ) -> Result<(), String>;
 
     fn deactivate_sensor(&mut self);
