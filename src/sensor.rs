6fca8ad91e0235abe091a10f9659105c18fe1453
danbulnet <daniel@bulanda.net>
2022-09-03 13:31:15 +0200

    add associated type to Sensor

--- src/sensor.rs
+++ src/sensor.rs
@@ -35,24 +35,26 @@ impl Distance for SensorDataType {
 impl SensorData for SensorDataType where {}
 
 pub trait Sensor where  {
+    type Data: SensorData;
+
     fn name(&self) -> &str;
 
     fn data_category(&self) -> DataCategory;
     
-    fn insert(&mut self, item: &SensorDataType) -> Rc<RefCell<dyn Neuron>>;
+    fn insert(&mut self, item: &Self::Data) -> Rc<RefCell<dyn Neuron>>;
     
-    fn search(&self, item: &SensorDataType) -> Option<Rc<RefCell<dyn Neuron>>>;
+    fn search(&self, item: &Self::Data) -> Option<Rc<RefCell<dyn Neuron>>>;
 
     fn activate(
         &mut self, 
-        item: &SensorDataType, 
+        item: &Self::Data, 
         signal: f32, 
         propagate_horizontal: bool, 
         propagate_vertical: bool
     ) -> Result<HashMap<NeuronID, Rc<RefCell<dyn Neuron>>>, String>;
     
     fn deactivate(
-        &mut self, item: &SensorDataType, propagate_horizontal: bool, propagate_vertical: bool
+        &mut self, item: &Self::Data, propagate_horizontal: bool, propagate_vertical: bool
     ) -> Result<(), String>;
 
     fn deactivate_sensor(&mut self);
