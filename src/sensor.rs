ccf20cd0427805b8b89af9b19284c49384a0f934
danbulnet <daniel@bulanda.net>
2022-09-04 01:54:35 +0200

    modify SensorDynamicDataMarker trait

--- src/sensor.rs
+++ src/sensor.rs
@@ -95,10 +95,10 @@ pub trait SensorDataFastMarker: Display + Distance + PartialEq + PartialOrd + Cl
 impl<T> SensorDataFastMarker for T 
 where T: Display + Distance + PartialEq + PartialOrd + Clone {}
 
-pub trait SensorDataDynamicMarker: SensorDataDynamic + 'static {}
+pub trait SensorDataDynamicMarker: SensorDataDynamic + PartialEq + PartialOrd + Clone + 'static {}
 
 impl<T> SensorDataDynamicMarker for T 
-where T: SensorDataDynamic + PartialEq + PartialOrd + 'static {}
+where T: SensorDataDynamic + PartialEq + PartialOrd + Clone + 'static {}
 
 pub trait SensorDynamicBuilder<Key: SensorDataDynamicMarker> {
     fn new(name: &str, data_category: DataCategory) -> Rc<RefCell<dyn SensorDynamic<Data = Key>>>;
