cc79373b38e76f4f0709de5f4aef8700e41f0927
danbulnet <daniel@bulanda.net>
2022-09-03 13:23:24 +0200

    combine SensorData trait with SensorDataType enum

--- src/sensor.rs
+++ src/sensor.rs
@@ -1,40 +1,58 @@
 use std::{
     rc::Rc,
     cell::RefCell,
-    collections::HashMap
+    collections::HashMap,
+    fmt::{ Display, Formatter, Result as FmtResult } 
 };
 
 use crate::{
     neuron::{ Neuron, NeuronID },
-    data::DataCategory
+    data::DataCategory,
+    distances::Distance
 };
 
-pub enum SensorData {
+pub trait SensorData: Clone + Display + PartialOrd + PartialEq + Distance {}
+
+#[derive(Clone, PartialEq, PartialOrd)]
+pub enum SensorDataType {
     I8(i8), I16(i16), I32(i32), I64(i64), I128(i128), ISize(isize),
     U8(u8), U16(u16), U32(u32), U64(u64), U128(u128), USize(usize),
-    F32(f32), F64(f64),
-    String(String), BoxStr(Box<str>), RcStr(Rc<str>)
+    F32(f32), F64(f64)
 }
 
-pub trait Sensor where {
+impl Display for SensorDataType {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", &self)
+    }
+}
+
+impl Distance for SensorDataType {
+    fn distance(&self, v: &Self) -> f64  {
+        if *self == *v { 0.0 } else { 1.0 }
+    }
+}
+
+impl SensorData for SensorDataType where {}
+
+pub trait Sensor where  {
     fn name(&self) -> &str;
 
     fn data_category(&self) -> DataCategory;
     
-    fn insert(&mut self, item: &SensorData) -> Rc<RefCell<dyn Neuron>>;
+    fn insert(&mut self, item: &SensorDataType) -> Rc<RefCell<dyn Neuron>>;
     
-    fn search(&self, item: &SensorData) -> Option<Rc<RefCell<dyn Neuron>>>;
+    fn search(&self, item: &SensorDataType) -> Option<Rc<RefCell<dyn Neuron>>>;
 
     fn activate(
         &mut self, 
-        item: &SensorData, 
+        item: &SensorDataType, 
         signal: f32, 
         propagate_horizontal: bool, 
         propagate_vertical: bool
     ) -> Result<HashMap<NeuronID, Rc<RefCell<dyn Neuron>>>, String>;
     
     fn deactivate(
-        &mut self, item: &SensorData, propagate_horizontal: bool, propagate_vertical: bool
+        &mut self, item: &SensorDataType, propagate_horizontal: bool, propagate_vertical: bool
     ) -> Result<(), String>;
 
     fn deactivate_sensor(&mut self);
