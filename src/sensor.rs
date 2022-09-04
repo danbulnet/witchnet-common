6dcc5b54f28fdd7f7a7e03a3b393d812792d7a05
danbulnet <daniel@bulanda.net>
2022-09-04 03:35:54 +0200

    remove Clone for the sake of DynClone

--- src/sensor.rs
+++ src/sensor.rs
@@ -97,10 +97,10 @@ pub trait SensorDataFastMarker: Display + Distance + PartialEq + PartialOrd + Cl
 impl<T> SensorDataFastMarker for T 
 where T: Display + Distance + PartialEq + PartialOrd + Clone {}
 
-pub trait SensorDataDynamicMarker: SensorDataDynamic + PartialEq + PartialOrd + Clone + 'static {}
+pub trait SensorDataDynamicMarker: SensorDataDynamic + PartialEq + PartialOrd + 'static {}
 
 impl<T> SensorDataDynamicMarker for T 
-where T: SensorDataDynamic + PartialEq + PartialOrd + Clone + 'static {}
+where T: SensorDataDynamic + PartialEq + PartialOrd + 'static {}
 
 pub trait SensorDynamicBuilder<Key: SensorDataDynamicMarker> {
     fn new(name: &str, data_category: DataCategory) -> Rc<RefCell<dyn SensorDynamic<Data = Key>>>;
