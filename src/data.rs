076241899ff4b30bb6ad912147e0cccc9f007240
danbulnet <daniel@bulanda.net>
2022-09-06 01:02:56 +0200

    impl From for DataCategory

--- src/data.rs
+++ src/data.rs
@@ -4,3 +4,33 @@ pub enum DataCategory {
     Categorical,
     Ordinal,
 }
+
+macro_rules! impl_numerical {
+    ( $($t:ty),* ) => {
+        $( impl From<&$t> for DataCategory {
+            fn from(_data: &$t) -> DataCategory { DataCategory::Numerical }
+        }
+        impl From<&[$t]> for DataCategory {
+            fn from(_data: &[$t]) -> DataCategory { DataCategory::Numerical }
+        }) *
+    }
+}
+
+macro_rules! impl_categorical {
+    ( $($t:ty),* ) => {
+        $( impl From<&$t> for DataCategory {
+            fn from(_data: &$t) -> DataCategory { DataCategory::Categorical }
+        }
+        impl From<&[$t]> for DataCategory {
+            fn from(_data: &[$t]) -> DataCategory { DataCategory::Categorical }
+        }) *
+    }
+}
+
+impl_numerical! { 
+    i8, i16, i32, i64, i128, isize,
+    u8, u16, u32, u64, u128, usize,
+    f32, f64
+}
+
+impl_categorical! { String, bool }
