b7e106eaccc0b65e36b42be52325ab43c6ffbc1f
danbulnet <daniel@bulanda.net>
2022-09-06 16:52:34 +0200

    Revert "add SensorData"
    This reverts commit 66b328d561e03daa41f006b393b3cdfb122eb87b.

--- src/sensor.rs
+++ src/sensor.rs
@@ -196,20 +196,3 @@ pub trait SensorFast {
 
     fn deactivate_sensor(&mut self);
 }
-
-
-pub enum SensorData {
-    Bool(bool),
-    U8(u8),
-    U16(u16),
-    U32(u32),
-    U64(u64),
-    I8(i8),
-    I16(i16),
-    I32(i32),
-    I64(i64),
-    F32(f32),
-    F64(f64),
-    String(Rc<str>),
-    Unknown
-}
