b9a184bc8754d1f2100f77c42c67c65414a462e4
danbulnet <daniel@bulanda.net>
2022-09-06 03:10:05 +0200

    add polars.rs to lib

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ pub mod data;
 pub mod algorithms;
 pub mod neuron;
 pub mod connection;
+pub mod polars;
--- src/polars.rs
+++ src/polars.rs
@@ -1,23 +1,30 @@
-use polars_core::series::Series;
+use std::fs::File;
 
-pub fn series_to_vec(series: &Series) -> SensorDataVec {
+use polars::prelude::*;
+
+use crate::data::DataVec;
+
+pub fn csv_to_dataframe(filename: &str) -> Result<DataFrame> {
+    let file = File::open(filename)?;
+    CsvReader::new(file).infer_schema(None).has_header(true).finish()
+}
+
+pub fn series_to_vec(series: &Series) -> DataVec {
     match series.dtype() {
-        DataType::Boolean => SensorDataVec::BoolVec(series.bool().unwrap().into_iter().collect()),
-        DataType::UInt8 => SensorDataVec::UInt8Vec(series.u8().unwrap().into_iter().collect()),
-        DataType::UInt16 => SensorDataVec::UInt16Vec(series.u16().unwrap().into_iter().collect()),
-        DataType::UInt32 => SensorDataVec::UInt32Vec(series.u32().unwrap().into_iter().collect()),
-        DataType::UInt64 => SensorDataVec::UInt64Vec(series.u64().unwrap().into_iter().collect()),
-        DataType::Int8 => SensorDataVec::Int8Vec(series.i8().unwrap().into_iter().collect()),
-        DataType::Int16 => SensorDataVec::Int16Vec(series.i16().unwrap().into_iter().collect()),
-        DataType::Int32 => SensorDataVec::Int32Vec(series.i32().unwrap().into_iter().collect()),
-        DataType::Int64 => SensorDataVec::Int64Vec(series.i64().unwrap().into_iter().collect()),
-        DataType::Float32 => 
-            SensorDataVec::Float32Vec(series.f32().unwrap().into_iter().collect()),
-        DataType::Float64 => 
-            SensorDataVec::Float64Vec(series.f64().unwrap().into_iter().collect()),
-        DataType::Utf8 => SensorDataVec::Utf8Vec(series.utf8().unwrap().into_iter().map(
+        DataType::Boolean => DataVec::BoolVec(series.bool().unwrap().into_iter().collect()),
+        DataType::UInt8 => DataVec::UInt8Vec(series.u8().unwrap().into_iter().collect()),
+        DataType::UInt16 => DataVec::UInt16Vec(series.u16().unwrap().into_iter().collect()),
+        DataType::UInt32 => DataVec::UInt32Vec(series.u32().unwrap().into_iter().collect()),
+        DataType::UInt64 => DataVec::UInt64Vec(series.u64().unwrap().into_iter().collect()),
+        DataType::Int8 => DataVec::Int8Vec(series.i8().unwrap().into_iter().collect()),
+        DataType::Int16 => DataVec::Int16Vec(series.i16().unwrap().into_iter().collect()),
+        DataType::Int32 => DataVec::Int32Vec(series.i32().unwrap().into_iter().collect()),
+        DataType::Int64 => DataVec::Int64Vec(series.i64().unwrap().into_iter().collect()),
+        DataType::Float32 => DataVec::Float32Vec(series.f32().unwrap().into_iter().collect()),
+        DataType::Float64 => DataVec::Float64Vec(series.f64().unwrap().into_iter().collect()),
+        DataType::Utf8 => DataVec::Utf8Vec(series.utf8().unwrap().into_iter().map(
             |v| match v { Some(val) => Some(val.to_string()), None => None }
         ).collect()),
-        _ => SensorDataVec::Unknown
+        _ => DataVec::Unknown
     }
 }
